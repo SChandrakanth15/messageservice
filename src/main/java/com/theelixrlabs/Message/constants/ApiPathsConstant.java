38aabb582fcad2a809b5e51ccf917f45fb073731
SChandrakanth15 <schandrakanth15@gmail.com>
2024-10-01 15:03:15 +0530

    updated with the constants and need to add the logging statements

---
+++ src/main/java/com/theelixrlabs/Message/constants/ApiPathsConstant.java
@@ -0,0 +1,9 @@
+package com.theelixrlabs.Message.constants;
+
+public class ApiPathsConstant {
+    public static final String MESSAGES_BASE = "/messages";
+    public static final String SEND_MESSAGE = "/send";
+    public static final String GET_USER_MESSAGES = "/user";
+    public static final String GET_CHAT_HISTORY = "/history/{selectedUsername}";
+
+}
---
+++ src/main/java/com/theelixrlabs/Message/constants/MessageConstant.java
@@ -0,0 +1,6 @@
+package com.theelixrlabs.Message.constants;
+
+public class MessageConstant {
+    public static final String DATE_TIME_FORMATTER = "dd:MM:yyyy HH:mm:ss";
+    public static final String MESSAGES_COLLECTION_NAME = "messages";
+}
